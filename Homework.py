330f2e59e9b047ab82313cff5d04c06780404746
ITSURGIRLHAJRAH <saadi.hajrajavaid@gmail.com>
2026-01-29 18:07:01 +0500

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,17 @@
+print("Hi, This is Sophia.\n")
+print("My birthday is in December 31st, and here are my other 5 friends` birthdays\n")
+
+laravar="November 3"
+print("Lara's birthday is on -", laravar)
+
+meganvar="Feburary 10"
+print("Megan's birthday is on -", meganvar)
+
+manonvar="November 3"
+print("Manon's birthday is on -", manonvar)
+
+yoonchaevar="June 26"
+print("Yoonchae's birthday is on -", yoonchaevar)
+
+danielavar="July 1"
+print("Daniela's birthday is on -", danielavar)
